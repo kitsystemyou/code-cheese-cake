dfdec19255e78c42eda629d48b32bce31ea3b154
kitsystemyou <kitsystemyou@gmail.com>
2020-09-12 23:01:02 +0900

    fuckin code-cheese-cake

---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+    Input strings {{ cheese }} View
+    </p>
+    <h2>AES</h2>
+    Transfered : 'cheesecake'
+    <h2>RSA</h2>
+    plain_text
+    <h2>WEP</h2>
+    plane_text
+  </div>
+</template>
+
+<script>
+var dummy="dummy"
+dummy += "errer"
+export default {
+  name: 'Code-CheeseCake',
+  props: {
+    msg: String,
+    cheese: dummy
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
